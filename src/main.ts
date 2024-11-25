cc9fb4724416395267a91c6998417e57c88e58da
Minyeong Jeong <megormin@gmail.com>
2024-11-25 11:41:00 +0100

    fetch function added

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,26 @@
-import './style.css'
-import { IQuestion } from './interface/IQuestion';
+import "./style.css";
+import { IQuestion } from "./interface/IQuestion";
 
+const BASE_URL = "https://vz-wd-24-01.github.io/typescript-quiz/questions/";
+const EN_EASY_ROUTE = `${BASE_URL}/easy.json`;
+const DE_EASY_ROUTE = `${BASE_URL}/leicht.json`;
+const EN_HARD_ROUTE = `${BASE_URL}/hard.json`;
+const DE_HARD_ROUTE = `${BASE_URL}/schwer.json`;
+
+let quizArr: IQuestion[] = [];
+
+async function fetchQuiz(URL: string) {
+  try {
+    const response: Response = await fetch(URL);
+    const data = await response.json();
+    // random questions
+    quizArr = data.sort(() => Math.random() - 0.5);
+    console.log("quizArr: ", quizArr);
+    return quizArr;
+  } catch (error) {
+    console.error(error);
+  }
+}
+// test
+fetchQuiz(EN_EASY_ROUTE);
+// test
